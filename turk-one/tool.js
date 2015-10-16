3fa32b366d68fdfbb15ce0afc19ce1d9e1eaf878
Sahand Nayebaziz <sahand@sahand.me>
2015-10-16 12:56:59 -0700

    add move button to toolbar

--- turk-one/tool.js
+++ turk-one/tool.js
@@ -54,24 +54,32 @@ if (Meteor.isClient) {
 			canvasArray = [];
 
 			// Set Black as default color in toolbar (set for each canvas at initialization in tool.html
+			Session.set("currentColor", "#414141");
 			$("#buttonBlack").addClass("selectedTool");
+
+			// setting toolbar active tool
+			function setSelectedTool(tool) {
+				$(".selectedTool").removeClass("selectedTool").addClass("deselectedTool");
+				$(tool).addClass("selectedTool");
+			}
 
 			// toolbar color clicked
 			$(".buttonColor").click(function () {
-				$(".selectedTool").removeClass("selectedTool");
-				$(this).addClass("selectedTool");
+				setSelectedTool(this);
 				var colorChosen = this.getAttribute("data-color");
 
+				Session.set("currentColor", colorChosen);
 				$.each(canvasArray, function() {
 					this.freeDrawingBrush.color = colorChosen;
+					this.isDrawingMode = true;
 				});
+
 				disableEraser();
 			});
 
 			// toolbar eraser clicked
 			$("#buttonEraser").click(function () {
-				$(".selectedTool").removeClass("selectedTool");
-				$(this).addClass("selectedTool");
+				setSelectedTool(this);
 				enableEraser();
 			})
 
@@ -101,16 +109,14 @@ if (Meteor.isClient) {
 				});
 			}
 
+			// toolbar move controls clicked
+			$("#buttonMove").click(function() {
+				setSelectedTool(this);
 
-
-
-			// enables the "submit" button in the quit survey once a reason is selected
-			$('input:radio').change(
-				function(){
-					$("#quitSubmit").removeClass("disabled");
-					$("#quitSubmit").prop("disabled",false);
-				}
-			);
+				$.each(canvasArray, function() {
+					this.isDrawingMode = false;
+				})
+			});
 
 			// UNDO AND REDO
 			canvasStateStack = [];
@@ -143,152 +149,13 @@ if (Meteor.isClient) {
 				}
 			});
 
-
-
-
-
-
-			//$.each(canvasArray, function() {
-			//	this.off("mouse:down");
-			//	this.isDrawingMode = true;
-			//});
-			//
-			//saveState();
-			//var recordingStates = true;
-			//
-			//canvas.on('object:added', function (e) {
-			//	//if (recordingStates) {
-			//	//	saveState();
-			//	//}
-			//})
-			//
-			//canvas.on('object:modified', function (e) {
-			//	//if (recordingStates) {
-			//	//	saveState();
-			//	//}
-			//})
-			//
-			//canvas.on('object:removed', function (e) {
-			//	//if (recordingStates) {
-			//	//	saveState();
-			//	//}
-			//})
-
-
-
-
-
-
-			/*
-			 // MARK: Create Canvas and add hooks
-			 canvas = new fabric.Canvas('practiceSketch');
-			 canvas.isDrawingMode = true;
-			 canvas.freeDrawingBrush.width = 6
-			 canvas.freeDrawingBrush.color = "black"
-
-			 var canvasStateStack = [];
-			 var canvasRedoStack = [];
-
-			 function saveState() {
-
-			 if (canvasStateStack.length == 30) {
-			 canvasStateStack.shift();
-			 }
-			 if (canvasRedoStack.length != 0) {
-			 canvasRedoStack = [];
-			 }
-			 canvasStateStack.push(JSON.stringify(canvas));
-			 }
-
-			 saveState();
-			 var recordingStates = true;
-
-			 canvas.on('object:added', function (e) {
-			 if (recordingStates) {
-			 saveState();
-			 }
-			 })
-
-			 canvas.on('object:modified', function (e) {
-			 if (recordingStates) {
-			 saveState();
-			 }
-			 })
-
-			 canvas.on('object:removed', function (e) {
-			 if (recordingStates) {
-			 saveState();
-			 }
-			 })
-
-			 // selecting colors
-			 $(".colorSelector").click(function () {
-			 $("img.selectedTool").removeClass("selectedTool");
-			 $(this).addClass("selectedTool");
-			 canvas.freeDrawingBrush.color = this.getAttribute("data-color");
-			 disableEraser();
-			 });
-
-			 // selecting the eraser
-			 $(".eraserTool").click(function () {
-			 $("img.selectedTool").removeClass("selectedTool");
-			 $(this).addClass("selectedTool");
-			 enableEraser();
-			 })
-
-			 function disableEraser() {
-			 canvas.off("mouse:down");
-			 canvas.isDrawingMode = true;
-			 }
-
-			 function enableEraser() {
-			 canvas.isDrawingMode = false;
-			 canvas.on("mouse:down", function (e) {
-			 if (canvas.getActiveGroup()) {
-			 recordingStates = false;
-			 canvas.getActiveGroup().forEachObject(function (a) {
-			 canvas.remove(a);
-			 });
-			 canvas.discardActiveGroup();
-			 recordingStates = true;
-			 } else {
-			 canvas.remove(canvas.getActiveObject());
-			 }
-			 canvas.renderAll();
-			 });
-			 }
-
-			 $(".undoSelector").click(function () {
-			 if (canvasStateStack.length > 1) {
-			 recordingStates = false;
-			 var currentState = canvasStateStack.pop();
-			 canvasRedoStack.push(currentState);
-
-			 var stateToReturnTo = canvasStateStack[canvasStateStack.length - 1];
-			 canvas.loadFromJSON(stateToReturnTo);
-			 canvas.renderAll();
-			 recordingStates = true;
-			 }
-			 });
-
-			 $(".redoSelector").click(function () {
-			 if (canvasRedoStack.length > 0) {
-			 recordingStates = false;
-			 var stateToReturnTo = canvasRedoStack.pop();
-			 canvasStateStack.push(stateToReturnTo);
-			 canvas.loadFromJSON(stateToReturnTo);
-			 canvas.renderAll();
-			 recordingStates = true;
-			 }
-			 });
-
-			 $(".clearSelector").click(function () {
-			 if (canvas.getObjects().length > 0) {
-			 canvas.clear().renderAll();
-			 saveState();
-			 }
-			 });
-			 */
+			// enables the "submit" button in the quit survey once a reason is selected
+			$('input:radio').change(
+				function(){
+					$("#quitSubmit").removeClass("disabled");
+					$("#quitSubmit").prop("disabled",false);
+				}
+			);
 
 		}
 	}
