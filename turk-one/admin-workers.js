f4498f19c5848e97e32f815bc9ed4242d92b5959
Sahand Nayebaziz <sahand@sahand.me>
2015-11-10 14:43:28 -0800

    admin filtering works for all combinations

--- turk-one/admin-workers.js
+++ turk-one/admin-workers.js
@@ -3,26 +3,26 @@
  */
 if (Meteor.isServer) {
 	Meteor.methods({
-		deleteTicket: function(id) {
+		deleteTicket: function (id) {
 			WorkerTickets.remove(id);
 		},
-		updateTicketReviewed: function(id, reviewedFlag) {
+		updateTicketReviewed: function (id, reviewedFlag) {
 			WorkerTickets.update(id, {
 				$set: {
 					reviewed: reviewedFlag
 				}
-			}, function(error) {
+			}, function (error) {
 				if (!error) {
 					console.log("set reviewed " + reviewedFlag);
 				}
 			})
 		},
-		updateSolutionStatus: function(id, newStatus) {
+		updateSolutionStatus: function (id, newStatus) {
 			Solutions.update(id, {
 				$set: {
 					status: newStatus
 				}
-			}, function(error) {
+			}, function (error) {
 				if (!error) {
 					console.log("solution status now " + newStatus);
 				}
@@ -36,36 +36,57 @@ if (Meteor.isClient) {
 	Meteor.subscribe("workerTickets");
 
 	// worker review page
+	function createMongoSelectorFromConditionals(arrayOfConditionals) {
+		if (arrayOfConditionals.length > 0) {
+			return {$or: arrayOfConditionals};
+		} else {
+			return {}
+		}
+	}
+
 	Template.adminWorkers.helpers({
 		workerTickets: function () {
-			if (!Session.get("shouldShowStatuses")) {
+			// TODO: Store session keys someplace so we don't have to remember the keys specifically each time
+			var statusesSelected = Session.get("shouldShowStatuses");
+
+			if (!statusesSelected) {
 				return WorkerTickets.find({}, {sort: {visited: -1}});
-			} else {
-				var filters = {};
-				console.log(Session.get("shouldShowStatuses"));
-				$.each(Session.get("shouldShowStatuses"), function(index, value) {
-					switch (value) {
+			}
+			else {
+				var conditionalsToAllow = [];
+
+				$.each(statusesSelected, function (i, status) {
+					switch (status) {
 						case "reviewed":
-							filters["reviewed"] = true;
+							conditionalsToAllow.push({"reviewed": true});
 							break;
 						case "submitted":
-							filters = {
+							conditionalsToAllow.push({
 								$and: [
 									{$or: [{"reviewed": false}, {"reviewed": {$exists: false}}]},
+									{$or: [{"quit": false}, {"quit": {$exists: false}}]},
 									{"submitted": true}
 								]
-							};
+							});
 							break;
 						case "in-progress":
-							filters["reviewed"] = {$exists: false};
-							filters["submitted"] = {$exists: false};
-							filters["quit"] = {$exists: false};
+								conditionalsToAllow.push({
+									$and: [
+										{"reviewed": {$exists: false}},
+										{"submitted": {$exists: false}},
+										{"quit": {$exists: false}}
+									]
+								});
+							break;
+						case "quit":
+							conditionalsToAllow.push({"quit": true});
 							break;
 						default:
-							filters = {};
+							break;
 					}
 				});
-				console.log(filters);
+
+				var filters = createMongoSelectorFromConditionals(conditionalsToAllow);
 				return WorkerTickets.find(filters, {sort: {visited: -1}});
 			}
 		}
@@ -122,14 +143,14 @@ if (Meteor.isClient) {
 				return "in-progress"
 			}
 		},
-		timeFormatted: function() {
+		timeFormatted: function () {
 			moment.tz.setDefault("America/Los_Angeles");
 			return moment(this.visited.toString()).calendar();
 		}
 	});
 
 	Template.workerTableControls.helpers({
-		filterShownClass: function(filter) {
+		filterShownClass: function (filter) {
 			var existingFilters = Session.get("shouldShowStatuses");
 			if (existingFilters) {
 				if ($.inArray(filter, existingFilters) == -1) {
@@ -144,7 +165,7 @@ if (Meteor.isClient) {
 	});
 
 	Template.workerTableControls.events({
-		"click .filter-control": function(e) {
+		"click .filter-control": function (e) {
 			var statusFilterToggled = $(e.target).attr('data-filter');
 			var newFilters = [];
 			if (Session.get("shouldShowStatuses")) {
@@ -179,7 +200,7 @@ if (Meteor.isClient) {
 		numberOfSolutionsRejected: function () {
 			return Solutions.find({workerId: this._id, status: "rejected"}).count();
 		},
-		isComplexEnough: function() {
+		isComplexEnough: function () {
 			return this.complexity > 0;
 		},
 		timeInToolInMinutes: function () {
@@ -195,15 +216,15 @@ if (Meteor.isClient) {
 
 	// TODO: make this a toggle instead of a set and cancel
 	Template.worker.events({
-		"click .reviewed": function() {
-			Meteor.call("updateTicketReviewed", this._id, true, function(e, r) {
+		"click .reviewed": function () {
+			Meteor.call("updateTicketReviewed", this._id, true, function (e, r) {
 				if (!e) {
 					Router.go("/admin");
 				}
 			});
 		},
-		"click .cancelReviewed": function() {
-			Meteor.call("updateTicketReviewed", this._id, false, function(e, r) {
+		"click .cancelReviewed": function () {
+			Meteor.call("updateTicketReviewed", this._id, false, function (e, r) {
 				if (!e) {
 					Router.go("/admin");
 				}
@@ -212,7 +233,7 @@ if (Meteor.isClient) {
 	});
 
 	Template.solutionReviewModule.helpers({
-		viewStatus: function() {
+		viewStatus: function () {
 			switch (this.status) {
 				case "pending":
 					return "";
@@ -230,13 +251,13 @@ if (Meteor.isClient) {
 	});
 
 	Template.solutionReviewModule.events({
-		"click .cancel": function() {
+		"click .cancel": function () {
 			Meteor.call("updateSolutionStatus", this._id, "pending");
 		},
-		"click .accept": function() {
+		"click .accept": function () {
 			Meteor.call("updateSolutionStatus", this._id, "accepted");
 		},
-		"click .reject": function() {
+		"click .reject": function () {
 			Meteor.call("updateSolutionStatus", this._id, "rejected");
 		}
 
