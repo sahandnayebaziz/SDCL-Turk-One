e5b6b593ca82fe92e1d38795bf8417161931a642
Edgar <edgar@dhcp-004068.ics.uci.edu>
2015-12-08 18:01:41 -0800

    find all solutions for a specific decision point
    new way to find all solutions for a specific decision point

--- turk-one/app/Views-Controllers/Admin/admin-review.js
+++ turk-one/app/Views-Controllers/Admin/admin-review.js
@@ -6,22 +6,47 @@ if (Meteor.isServer) {
 
 if (Meteor.isClient) {
 
+    reviewNumber = 2;
+
     Template.adminQualityReviewInterior.helpers({
 
         solutions: function () {
-            return Solutions.find({
-                    submitted: true,
+            var workers = WorkerTickets.find({
+                decisionPointId: this._id
+            }).fetch();
+
+            var tickets = _.chain(workers)
+                .pluck('_id')
+                .flatten()
+                .uniq()
+                .value();
+
+            var results = Solutions.find({
                     status: "accepted",
-                    decisionPointId: this._id
+                    workerId: {$in: tickets}
                 },
                 {
                     sort: {_id: -1} //sort of random sorting
-                });
+                }).fetch();
+
+            console.log("results: " +results);
+
+            return results;
         },
 
         decisionPoints: function () {
             // find all decisions, sorted by id
             return DecisionPoints.find({}, {sort: {_id: 1}});
+        },
+
+        reviewNumber: function () {
+            console.log('reviewNumber: ' +reviewNumber);
+            reviewNumber += 1;
+            return reviewNumber;
+        },
+
+        resetNumbering: function () {
+            reviewNumber=2;
         }
     });
 }
